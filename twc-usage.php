8fba4d8b418ec75660e0ef8a742664de91d9ac14
Brendan DeBeasi <b@cyber2.com>
2015-12-30 18:34:04 -0800

    Initial working integration1

--- twc-usage.php
+++ twc-usage.php
@@ -25,7 +25,7 @@ curl_exec($ch);
 
 
 //Submit login info
-curl_setopt($ch, CURLOPT_URL, 'https://myservices.timewarnercable.com/login/');
+curl_setopt($ch, CURLOPT_URL, 'https://myservices.timewarnercable.com/myservices/login/validate');
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, array(
 	'username'=>$config['username'],
@@ -35,35 +35,33 @@ $login1 = curl_exec($ch);
 
 //Wayfarer login
 curl_setopt($ch, CURLOPT_URL, 'https://wayfarer.timewarnercable.com/wayfarer');
-curl_setopt($ch, CURLOPT_REFERER, 'https://myservices.timewarnercable.com/login/');
+curl_setopt($ch, CURLOPT_REFERER, 'https://myservices.timewarnercable.com/login/validate');
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
 	'agent'=>'mys',
 	'Ecom_User_ID'=>$config['username'],
 	'Ecom_Password'=>$config['password'],
 	'errorUrl'=>'https://myservices.timewarnercable.com/login/validate?loginFailed=true',
 	'originalResourceUrl'=>base64_encode('https://myservices.timewarnercable.com/account/index?jli=true'),
-));
+)));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 	'Connection'=>'keep-alive',
 	'Cache-Control'=>'max-age=0',
 	'Accept'=>'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 	'Origin'=>'https://myservices.timewarnercable.com',
 	'Upgrade-Insecure-Requests'=>'1',
-	'Content-Type'=>'application/x-www-form-urlencoded',
 	'Accept-Encoding'=>'gzip, deflate',
-	'Accept-Language'=>'en-US,en;q=0.8'
+	'Accept-Language'=>'en-US,en;q=0.8',
 ));
 
 $login2 = curl_exec($ch);
 preg_match('~<input type="hidden" name="SAMLRequest" value="(.*?)"~',$login2,$saml);
 if(isset($saml[1])) $saml = str_replace(PHP_EOL,'',trim($saml[1]));
-//echo $login2;die();
 
 //SAML Request
 curl_setopt($ch, CURLOPT_URL, 'https://ids.rr.com/nidp/saml2/sso');
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
 	'SAMLRequest'=>$saml,
 	'RelayState'=>base64_encode('https://myservices.timewarnercable.com/account/index?jli=true'),
 	'agent'=>'mys',
@@ -71,7 +69,7 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, array(
 	'Ecom_User_ID'=>$config['username'],
 	'errorUrl'=>'https://myservices.timewarnercable.com/login/validate?loginFailed=true',
 	'Ecom_Password'=>$config['password'],
-));
+)));
 curl_setopt($ch, CURLOPT_REFERER, 'https://wayfarer.timewarnercable.com/wayfarer');
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 	'Connection'=>'keep-alive',
@@ -79,54 +77,58 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 	'Accept'=>'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 	'Origin'=>'https://wayfarer.timewarnercable.com',
 	'Upgrade-Insecure-Requests'=>'1',
-	'Content-Type'=>'application/x-www-form-urlencoded',
 	'Accept-Encoding'=>'gzip, deflate',
-	'Accept-Language'=>'en-US,en;q=0.8'
+	'Accept-Language'=>'en-US,en;q=0.8',
 ));
-$saml_req = curl_exec($ch);
-echo $saml_req;die();
 
-
+$saml_resp = curl_exec($ch);
+preg_match('~<input type="hidden" name="SAMLResponse" value="(.*?)"~',$saml_resp,$saml);
+if(isset($saml[1])) $saml = str_replace(PHP_EOL,'',trim($saml[1]));
 
 //SAML POST
 curl_setopt($ch, CURLOPT_URL, 'https://wayfarer.timewarnercable.com/wayfarer/SAML2/POST');
-curl_setopt($ch, CURLOPT_REFERER, 'https://wayfarer.timewarnercable.com/wayfarer');
+curl_setopt($ch, CURLOPT_REFERER, 'https://ids.rr.com/nidp/saml2/sso?sid=0');
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
 	'SAMLResponse'=>$saml,
-	'RelayState'=>base64_encode('https://myservices.timewarnercable.com/account/index?jli=true')
-));
+	'RelayState'=>base64_encode('https://myservices.timewarnercable.com/account/index?jli=true'),
+	'x'=>75,
+	'y'=>12
+)));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-	'Content-Type'=> 'application/x-www-form-urlencoded',
-	'Origin'=> 'https://wayfarer.timewarnercable.com',
+	'Connection'=>'keep-alive',
+	'Cache-Control'=>'max-age=0',
+	'Accept'=>'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'Origin'=>'https://wayfarer.timewarnercable.com',
+	'Upgrade-Insecure-Requests'=>'1',
+	'Accept-Encoding'=>'gzip, deflate',
+	'Accept-Language'=>'en-US,en;q=0.8',
 ));
+
+
 $saml = curl_exec($ch);
-echo $saml;
-die();
 
 
-//Do SSO
-curl_setopt($ch, CURLOPT_URL, 'https://ids.rr.com/nidp/saml2/sso');
-curl_setopt($ch, CURLOPT_REFERER, 'https://wayfarer.timewarnercable.com/wayfarer');
-curl_setopt($ch, CURLOPT_HTTPHEADER, array());
-curl_setopt($ch, CURLOPT_POST, false);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-
-$sso = curl_exec($ch);
-echo $sso;
-die();
 //Get Modem Info
 curl_setopt($ch, CURLOPT_URL, 'https://myservices.timewarnercable.com/internet/api_modem_info');
 curl_setopt($ch, CURLOPT_REFERER, 'https://myservices.timewarnercable.com/myservices/internet/index');
 curl_setopt($ch, CURLOPT_POST, false);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 
-$modem_info = curl_exec($ch);
-echo $modem_info;
-//
-//$usage = curl_exec($ch);
-//if(empty($config['out'])) echo $usage;
-//else file_put_contents($config['out'], $usage);
-//
-//unlink('/tmp/twcauth.txt');
-//curl_close($ch);
+$modems = json_decode(curl_exec($ch),true);
+
+foreach($modems as &$modem) {
+	curl_setopt($ch, CURLOPT_URL, 'https://myservices.timewarnercable.com/internet/api_modem_usage?mac='.$modem['mac'].'&mode=currentMonth');
+	curl_setopt($ch, CURLOPT_REFERER, 'https://myservices.timewarnercable.com/myservices/internet/index');
+	curl_setopt($ch, CURLOPT_POST, false);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+	$usage = curl_exec($ch);
+	$modem['usage'] = $usage['result']['usage'];
+}
+
+
+if(empty($config['out'])) echo $usage;
+else file_put_contents($config['out'], $usage);
+
+unlink('/tmp/twcauth.txt');
+curl_close($ch);
